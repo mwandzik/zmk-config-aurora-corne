3a5cf072812f8b19c8170b3a576ab75854b891e9
Michael <michael.wandzik@gmail.com>
2025-04-20 23:29:35 +0200

    Fix module integration and Kconfig issues

--- config/modules/zmk-driver-pim447/include/dt-bindings/zmk/trackball_pim447.h
+++ config/modules/zmk-driver-pim447/include/dt-bindings/zmk/trackball_pim447.h
@@ -6,20 +6,20 @@
 #pragma once
 
 /* Trackball modes */
-#define PIM447_MOVE    0
-#define PIM447_SCROLL  1
+#define PIM447_MOVE 0
+#define PIM447_SCROLL 1
 
 /* Trackball mode toggle options */
-#define MOVE_TOGGLE    0
-#define SCROLL_SET     1
-#define MOVE_SET       2
+#define MOVE_TOGGLE 0
+#define SCROLL_SET 1
+#define MOVE_SET 2
 
 /* LED colors (preset options) */
-#define LED_OFF        0
-#define LED_RED        1
-#define LED_GREEN      2
-#define LED_BLUE       3
-#define LED_YELLOW     4
-#define LED_CYAN       5
-#define LED_MAGENTA    6
-#define LED_WHITE      7
+#define LED_OFF 0
+#define LED_RED 1
+#define LED_GREEN 2
+#define LED_BLUE 3
+#define LED_YELLOW 4
+#define LED_CYAN 5
+#define LED_MAGENTA 6
+#define LED_WHITE 7
